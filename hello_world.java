6550aab05cb8ca71cbf6e08a58e969bf9fd4c603
Bala-darshan <baladarshan7@gmail.com>
2025-06-12 14:43:18 +0530

    Add hello_world.java file
    This is a simple Java program that prints "Hello, World!" to the console.

---
+++ hello_world.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class hello_world
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!"); 
+    }
+}
+
+// To run this program in vs code or terminal use the line
+// step 1: javac filename.java (used to compile a code)
+// step 2: java filename(used to convert it into a byte code)
