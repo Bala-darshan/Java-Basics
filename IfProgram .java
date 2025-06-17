4221a07ba2419b81f8fef9bcc337697aa4803a7d
Bala-darshan <baladarshan7@gmail.com>
2025-06-17 12:27:49 +0530

    if_program.java
    The if_program demonstrates the use of a basic if condition in Java.

---
+++ IfProgram .java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+class IfProgram  // Class name should ideally follow PascalCase naming convention
+{
+    public static void main(String[] args)
+    {
+        // Declare and initialize the variable
+        int a = 5;
+
+        // Check if 'a' is equal to 5
+        if (a == 5)
+        {
+            // If the condition is true, print the message
+            System.out.println("Value 5 is stored in variable 'A'");
+        }
+    }
+}
+
+//output : Value 5 is stored in variable 'A'
